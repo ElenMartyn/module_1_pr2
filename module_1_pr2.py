3c67f9b731573b144dca479a28a8771bed4c4684
ElenMartyn <elenamartyn00@gmail.com>
2024-10-09 22:22:09 +0700

    Add files via upload

---
+++ module_1_pr2.py
@@ -0,0 +1,11 @@
+course_name = 'Pyton' #название курса
+print(course_name)
+totalTasks= 12 #Количество выполненных ДЗ
+print(totalTasks)
+hoursSpent = 1.5  #Количество затраченных часов
+average_time = hoursSpent/ totalTasks
+print(average_time)
+print('Курс:',course_name,'Всего задач:',totalTasks,'Затрачено часов:',hoursSpent,'Среднее время выполнения',average_time)
+
+
+
